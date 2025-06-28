1065da9824407106d6465a9bd2f64c9d31fc0e54
ATHI4545 <athikuti4@gmail.com>
2025-06-28 20:03:36 +0530

    lastword

---
+++ 28.06.25/length_of_last.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        String[] st = s.split(" ");
+        String str = st[st.length-1];
+        int n = str.length(); 
+    return n;
+    }
+    }
