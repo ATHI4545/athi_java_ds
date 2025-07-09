98f20021cabfdd49818c1a5a23f03b8080a549d2
ATHI4545 <athikuti4@gmail.com>
2025-07-09 10:54:07 +0530

    reverse

--- 8.07.25/Main.java
+++ 8.07.25/Main.java
@@ -22,6 +22,47 @@ public void insert(int data){
                 tail=n;
             }
         }
+        public void insertAtpos(int pos,int data){
+            if(pos==0){
+                Node newnode=new Node(data);
+                newnode.next=head;
+                head=newnode;
+                return;
+            }
+            Node newnode=new Node(data);
+            Node temp=head;
+            for(int i=1;i<pos;i++){
+                temp=temp.next;
+            }
+            newnode.next=temp.next;
+            temp.next=newnode;
+        }
+        public void deleteval(int data){
+            if(head==null){
+               System.out.println("List is empty");
+            }
+            if(head.data==data){
+                head=head.next;
+                return;
+            }
+            Node prev=head;
+            Node temp=head.next;
+            while(temp!=null){
+                if(temp.data==data){
+                    prev.next=temp.next;
+                    
+                }
+            }
+        }
+        public void delete(int pos){
+            Node prev=null;
+            Node temp=head;
+            for(int i=1;i<=pos;i++){
+                prev=temp;
+                temp=temp.next; 
+            }
+            prev.next=temp.next;
+        }
 
 public void display(){
     Node temp=head;
@@ -38,6 +79,10 @@ public class Main{
         l.insert(10);
         l.insert(20);
         l.insert(30);
+        l.insertAtpos(2,40);
+        l.insertAtpos(0,70);
+        l.delete(2);
+        l.deleteval(70);
         l.display();
     }
 }
